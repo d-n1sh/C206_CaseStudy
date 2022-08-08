6f354d819b762b0ac60dc5fc516cc108c6785dd0
13771 <13771@10.174.123.82>
2022-08-08 15:45:08 +0800

    parents classssssss

---
+++ C206_CaseStudy/src/Parents.java
@@ -0,0 +1,22 @@
+
+public class Parents {
+	
+	private String parentsname;
+	private int id;
+	
+					
+	public String getParentsname() {
+		return parentsname;
+	}
+	public void setParentsname(String parentsname) {
+		this.parentsname = parentsname;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
