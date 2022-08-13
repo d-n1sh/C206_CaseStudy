0c975d64272f83cd725e856f72ec5f15b4442db7
Danis <Danis@Danish>
2022-08-13 15:21:52 +0800

    Updated main and parent class

--- C206_CaseStudy/src/Parents.java
+++ C206_CaseStudy/src/Parents.java
@@ -1,21 +1,92 @@
+import java.util.Random;
 
 public class Parents {
 	
-	private String parentsname;
-	private int id;
+	private String studentid;
+	private String studentName;
+	private String studentClass;
+	private String studentGrade;
+	private String studentTeacher;
+	private String parentsName;
+	private String parentsEmail;
+	private String parentsContact;
+	private String regID;
 	
-					
-	public String getParentsname() {
-		return parentsname;
+	public Parents(String studentid, String parentsName, String parentsEmail, String parentsContact, String regID) {
+		super();
+		this.studentid = studentid;
+		this.parentsName = parentsName;
+		this.parentsEmail = parentsEmail;
+		this.parentsContact = parentsContact;
+		this.regID = regID;
 	}
-	public void setParentsname(String parentsname) {
-		this.parentsname = parentsname;
+
+	public String getStudentid() {
+		return studentid;
 	}
-	public int getId() {
-		return id;
+	
+	public void setStudentid(String studentid) {
+		this.studentid = studentid;
 	}
-	public void setId(int id) {
-		this.id = id;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	
+	public String getStudentClass() {
+		return studentClass;
+	}
+	
+	public void setStudentClass(String studentClass) {
+		this.studentClass = studentClass;
+	}
+	
+	public String getStudentGrade() {
+		return studentGrade;
+	}
+	
+	public void setStudentGrade(String studentGrade) {
+		this.studentGrade = studentGrade;
+	}
+	
+	public String getStudentTeacher() {
+		return studentTeacher;
+	}
+	
+	public void setStudentTeacher(String studentTeacher) {
+		this.studentTeacher = studentTeacher;
+	}
+	
+	public String getParentsName() {
+		return parentsName;
+	}
+	
+	public void setParentsName(String parentsName) {
+		this.parentsName = parentsName;
+	}
+	
+	public String getParentsEmail() {
+		return parentsEmail;
+	}
+	
+	public void setParentsEmail(String parentsEmail) {
+		this.parentsEmail = parentsEmail;
+	}
+	
+	public String getParentsContact() {
+		return parentsContact;
+	}
+	
+	public void setParentsContact(String  parentsContact) {
+		this.parentsContact = parentsContact;
+	}
+	
+	public String getRegID() {
+		return regID;
 	}
 	
 }
